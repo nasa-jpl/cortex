ed6c51f6e47a5e68d54f975c63c1f528a276cbeb
Rob Royce <rob.royce@jpl.nasa.gov>
2024-02-13 15:19:59 -0800

    Add initial Annotator agent.

--- src/cortex/agents/__init__.py
+++ src/cortex/agents/__init__.py
@@ -14,5 +14,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from .worker import Worker
+from .annotator import Annotator, AnnotationLevel, AnnotationTags
 from .monitor import Monitor
+from .worker import Worker
