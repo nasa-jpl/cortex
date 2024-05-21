a4afeebe6d74f7c89ad2bfa3f1c726b049b36a6d
Rob Royce <rob.royce@jpl.nasa.gov>
2024-05-21 11:58:53 -0700

    fix: an issue where nodes running on a different device show up in the list of topics but PID cannot be found.

--- ros1/scripts/monitor.py
+++ ros1/scripts/monitor.py
@@ -38,7 +38,10 @@ class ROS1Monitor(CRTXMonitor):
         # Get an initial list of ROS nodes and their PIDs
         pids = ROS1Utils.get_node_pids(hash=CacheUtils.expire_by_ttl())
         for node, pid in pids.items():
-            self.add_node(pid, node)
+            try:
+                self.add_node(pid, node)
+            except Exception as e:
+                rospy.logerr(f"Error adding node (ignoring) {node}: {e}")
         self.start()
 
         while not rospy.is_shutdown():
@@ -58,8 +61,8 @@ class ROS1Monitor(CRTXMonitor):
                 for node in added:
                     try:
                         self.add_node(new_pids[node], node)
-                    except ValueError as e:
-                        rospy.logerr(f"Error adding node {node}: {e}")
+                    except Exception as e:
+                        rospy.logerr(f"Error adding node (ignoring) {node}: {e}")
                         del new_pids[node]
                 for node in removed:
                     self.remove_node(node)
